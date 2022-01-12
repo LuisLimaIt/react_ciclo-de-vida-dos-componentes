c1486c3da39f1ae7a2d1630818554e0597a6f5d5
Luis Paulo de Lima Soares <pls.luis@gmail.com>
2022-01-12 12:35:44 -0300

    Adicionando estado local

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,7 +6,16 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo = "";
     this.texto = "";
-    this.categoria = "";
+    this.categoria = "Sem Categoria";
+    this.state = { categorias: [] }
+  }
+
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+  }
+
+  _novasCategorias(categorias) {
+    this.setState({ ...this.state, categorias });
   }
 
   _handleMudancaCategoria(evento) {
@@ -41,9 +50,10 @@ class FormularioCadastro extends Component {
           className="form-cadastro_input"
         >
           <option>Sem categoria</option>
-          {this.props.categorias.map((categoria) => {
-            return <option>{categoria}</option>
-          })}
+
+          {this.state.categorias.map((categoria, index) => {
+            return <option key={index}>{categoria}</option>
+           })}
         </select>
         <input
           type="text"
--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -1,15 +1,18 @@
 export default class ArrayDeNotas {
     constructor() {
         this.notas = [];
+        this._inscritos = [];
     }
 
     adicionarNota(titulo, texto, categoria) {
         const novaNota = new Nota(titulo, texto, categoria);
         this.notas.push(novaNota);
+        this.notificar();
     }
 
     apagarNota(indice) {
         this.notas.splice(indice, 1);
+        this.notificar();
     }
 
     inscrever(func) {
@@ -18,7 +21,7 @@ export default class ArrayDeNotas {
 
     notificar() {
         this._inscritos.forEach(func => {
-            func(this.categorias);
+            func(this.notas);
         });
     }
 }
